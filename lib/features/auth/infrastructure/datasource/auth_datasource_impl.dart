f0b3b62a5272db865388fdefe82b557197bc1421
Franco Inostroza <franco.inostrozah@gmail.com>
2023-11-21 18:51:38 -0300

    :sparkles: (auth - login) implmentation of authdatasource

--- lib/features/auth/infrastructure/datasource/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasource/auth_datasource_impl.dart
@@ -1,6 +1,11 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_shop/config/constanst/environment.dart';
 import 'package:teslo_shop/features/auth/domain/domain.dart';
+import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
 
 class AuthDataSourceImpl extends AuthDataSource {
+  final dio = Dio(BaseOptions(baseUrl: Environment.apiUrl));
+
   @override
   Future<User> checkAuthStatus(String token) {
     // TODO: implement checkAuthStatus
@@ -8,9 +13,17 @@ class AuthDataSourceImpl extends AuthDataSource {
   }
 
   @override
-  Future<User> login(String email, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<User> login(String email, String password) async {
+    try {
+      final response = await dio
+          .post('auth/login', data: {'email': email, 'password': password});
+
+      final user = UserMapper.userJsonEntity(response.data);
+
+      return user;
+    } catch (e) {
+      throw UnimplementedError();
+    }
   }
 
   @override
--- lib/features/auth/infrastructure/infrastructure.dart
+++ lib/features/auth/infrastructure/infrastructure.dart
@@ -1,2 +1,4 @@
 export 'datasource/auth_datasource_impl.dart';
 export 'repositories/auth_repository_impl.dart';
+export 'mappers/user_mapper.dart';
+export 'errors/auth_errors.dart';
