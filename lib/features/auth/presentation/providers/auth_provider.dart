7007c8e71d0b82c7c8482ec53678ab13fb62037f
Franco Inostroza <franco.inostrozah@gmail.com>
2023-11-21 19:11:19 -0300

    :sparkles: (auth)  login and logout from provider

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -13,9 +13,32 @@ class AuthNotifier extends StateNotifier<AuthState> {
 
   AuthNotifier({required this.authRepository}) : super(AuthState());
 
-  void loginUser(String email, String password) async {}
+  void loginUser(String email, String password) async {
+    await Future.delayed(const Duration(milliseconds: 500));
+
+    try {
+      final user = await authRepository.login(email, password);
+      _setLoggedUser(user);
+    } on WrongCredentials {
+      logout('Credenciales no son correctas');
+    } catch (e) {
+      logout('Error no controlado');
+    }
+  }
+
   void registerUser(String email, String password) async {}
   void checkAuthStatus() async {}
+
+  Future<void> logout([String? errorMessage]) async {
+    state = state.copyWith(
+        authStatus: AuthStatus.notAuthenticated,
+        user: null,
+        errorMessage: errorMessage);
+  }
+
+  void _setLoggedUser(User user) {
+    state = state.copyWith(user: user, authStatus: AuthStatus.authenticated);
+  }
 }
 
 enum AuthStatus { checking, authenticated, notAuthenticated }
@@ -31,7 +54,7 @@ class AuthState {
       this.errorMessage = ''});
 
   AuthState copyWith(
-          AuthStatus? authStatus, User? user, String? errorMessage) =>
+          {AuthStatus? authStatus, User? user, String? errorMessage}) =>
       AuthState(
           authStatus: authStatus ?? this.authStatus,
           user: user ?? this.user,
